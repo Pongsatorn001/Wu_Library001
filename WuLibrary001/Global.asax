9a41d9033c1951cb11bca6ba6cd47b63bbb137d7
DESKTOP-QQ4UMN4\asus <Pongsatorn698@gmail.com>
2018-01-17 02:04:02 +0700

    Add project files.

---
+++ WuLibrary001/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WuLibrary001.MvcApplication" Language="C#" %>
---
+++ WuLibrary001/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WuLibrary001.Startup))]
+namespace WuLibrary001
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
